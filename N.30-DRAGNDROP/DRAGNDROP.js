08d8d0c7626ec05577b009a155c84d6ee69d221f
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2023-11-01 16:23:55 +0300

    added function

---
+++ N.30-DRAGNDROP/DRAGNDROP.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+  findElements();
+};
+
+function findElements() {
+  const array = document.getElementsByClassName("move");
+  for (let i = array.length - 1; i >= 0; i--) {
+    array[i].style.position = "absolute";
+    array[i].style.zIndex = 1;
+    let coords = array[i].getBoundingClientRect();
+    array[i].style.left = coords.left + "px";
+    array[i].style.top = coords.top + "px";
+
+    dragItem(array[i]);
+  }
+}
+
+function dragItem(e) {
+  e.onmousedown = function (event) {
+    shiftX = event.clientX - e.getBoundingClientRect().left;
+    shiftY = event.clientY - e.getBoundingClientRect().top;
+
+    e.classList.add("totake");
+
+    e.style.position = "absolute";
+    document.body.append(e);
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+      e.style.left = pageX - shiftX + "px";
+      e.style.top = pageY - shiftY + "px";
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+
+    e.onmouseup = function () {
+      document.removeEventListener("mousemove", onMouseMove);
+      e.classList.remove("totake");
+      e.onmouseup = null;
+    };
+  };
+
+  e.ondragstart = function () {
+    return false;
+  };
+}
