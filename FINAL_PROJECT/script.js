990024b636c7ed29c7005e6fc11fb612f6b7eab6
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2023-12-12 12:57:43 +0300

    optimizated func

--- FINAL_PROJECT/script.js
+++ FINAL_PROJECT/script.js
@@ -4,29 +4,32 @@ function game() {
     let isPause = false;
     let animationId = null;
 
+    console.log(window);
+
     const speed = 3;
 
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.tree');
 
     const tree1 = trees[0]; // достал дерево
+    const cordsTree1 = getCords(tree1); //достаю координаты первого дерева
 
     animationId = requestAnimationFrame(startGame); //анимация
 
     function startGame() { // запуск игры
+        // treesAnimation(); //анимация деревьев
 
-        treesAnimation(); //анимация деревьев
-
-        animationId = requestAnimationFrame(startGame);
+        // animationId = requestAnimationFrame(startGame);
     };
 
     function treesAnimation() { //анимация деревьев
-        const coords = getCords(tree1); //достаю координаты первого дерева
-        const newCoordY = coords.y + speed; //смена координат, каждый раз плюс скорость
-        tree1.style.transform = `translate(${coords.x}px, ${newCoordY}px)`; // ставлю новое значение координат
+        const newCoordY = cordsTree1.y + speed; //смена координат, каждый раз плюс скорость
+        cordsTree1.y = newCoordY; //меняю только у координату
+        tree1.style.transform = `translate(${cordsTree1.x}px, ${newCoordY}px)`; // ставлю новое значение координат
     }
 
     function getCords(element) { //достаю координаты элемента
+        console.log(element);
         const matrix = window.getComputedStyle(element).transform; // достаю стиль
         const array = matrix.split(','); //по запятым
         const y = array[array.length - 1]; //последний элемент
@@ -46,6 +49,7 @@ function game() {
             gameButton.children[1].style.display = 'initial';
         }
         else {
+            animationId = requestAnimationFrame(startGame); // запуск после паузы
             gameButton.children[0].style.display = 'initial';
             gameButton.children[1].style.display = 'none';
         }
