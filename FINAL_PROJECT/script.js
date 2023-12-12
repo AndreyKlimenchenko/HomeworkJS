3aa2babf5d1c6bca4ad93bd528d7e50d5f74bade
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2023-12-12 16:10:08 +0300

    added car animation

--- FINAL_PROJECT/script.js
+++ FINAL_PROJECT/script.js
@@ -4,18 +4,98 @@ function game() {
     let isPause = false;
     let animationId = null;
 
-    const speed = 10;
+    const speed = 3;
 
     const car = document.querySelector('.car');
     const trees = document.querySelectorAll('.tree');
 
+    const carCoords = getCoords(car);
+    const carMoveInfo = {
+        top: null,
+        bottom: null,
+        left: null,
+        right: null,
+    };
+
     const treesCoords = [];
+
 
     for(let i = 0; i < trees.length; i++) {
         const tree = trees[i];
-        const coordsTree = getCords(tree);
+        const coordsTree = getCoords(tree);
 
         treesCoords.push(coordsTree); // добавляю координаты в массив
+    }
+
+    document.addEventListener('keydown', (event) => {
+        const code = event.code;
+
+        if (code === 'ArrowUp' && carMoveInfo.top === null) {
+            carMoveInfo.top = requestAnimationFrame(carMoveToTop); 
+        }
+        else if (code === 'ArrowDown' && carMoveInfo.bottom === null) {
+            carMoveInfo.bottom = requestAnimationFrame(carMoveToBottom); 
+        }
+        else if (code === 'ArrowLeft' && carMoveInfo.left === null) {
+            carMoveInfo.left = requestAnimationFrame(carMoveToLeft); 
+        }
+        else if (code === 'ArrowRight' && carMoveInfo.right === null) {
+            carMoveInfo.right = requestAnimationFrame(carMoveToRight); 
+        }
+    });
+
+    document.addEventListener('keyup', (event) => {
+        const code = event.code;
+
+        if (code === 'ArrowUp') {
+            cancelAnimationFrame(carMoveInfo.top);
+            carMoveInfo.top === null;
+        }
+        else if (code === 'ArrowDown') {
+            cancelAnimationFrame(carMoveInfo.bottom);
+            carMoveInfo.bottom === null;
+        }
+        else if (code === 'ArrowLeft') {
+            cancelAnimationFrame(carMoveInfo.left);
+            carMoveInfo.left === null;
+        }
+        else if (code === 'ArrowRight') {
+            cancelAnimationFrame(carMoveInfo.right);
+            carMoveInfo.right === null;
+        }
+    });
+
+
+    function carMoveToTop() { // расчет координат машины
+        const newY = carCoords.y - 5;
+        carCoords.y = newY;
+        carMove(carCoords.x, newY);
+        carMoveInfo.top = requestAnimationFrame(carMoveToTop); 
+    }
+
+    function carMoveToBottom() { // расчет координат машины
+        const newY = carCoords.y + 5;
+        carCoords.y = newY;
+        carMove(carCoords.x, newY);
+        carMoveInfo.bottom = requestAnimationFrame(carMoveToBottom);
+    }
+
+    function carMoveToLeft() { // расчет координат машины
+        const newX = carCoords.x - 5;
+        carCoords.x = newX;
+        carMove(newX, carCoords.y);
+        carMoveInfo.left = requestAnimationFrame(carMoveToLeft); 
+    }
+
+    function carMoveToRight() { // расчет координат машины
+        const newX = carCoords.x + 5;
+        carCoords.x = newX;
+        carMove(newX, carCoords.y);
+        carMoveInfo.right = requestAnimationFrame(carMoveToRight); 
+    }
+
+    function carMove(x, y) {
+        car.style.transform = `translate(${x}px, ${y}px)`; // ставлю новое значение координат
     }
 
     animationId = requestAnimationFrame(startGame); //анимация
@@ -46,7 +126,7 @@ function game() {
        
     }
 
-    function getCords(element) { //достаю координаты элемента
+    function getCoords(element) { //достаю координаты элемента
         const matrix = window.getComputedStyle(element).transform; // достаю стиль
         const array = matrix.split(','); //по запятым
         const y = array[array.length - 1]; //последний элемент
